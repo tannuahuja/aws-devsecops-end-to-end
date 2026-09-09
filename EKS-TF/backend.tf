ece544c9bf843309b7d4f7a2147475dbfe1a178e
TA <59529688+tannuahuja@users.noreply.github.com>
2026-09-09 21:42:48 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "dev-aman-tf-bucket"
+    bucket       = "dev-tannu-tf-bucket"
     region       = "us-east-1"
     key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
     use_lockfile = true
